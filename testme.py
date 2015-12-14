ad3fce1b54a878b72696790fa9085a19e2caed9c
Darius Bacon <darius@wry.me>
2015-12-14 15:14:10 -0500

    rename terp to first_terp

--- testme.py
+++ testme.py
@@ -3,11 +3,11 @@ Smoke test
 """
 
 from parse import parse_toot
-from terp import eval_program
+import first_terp as terp
 
 def run(program_text):
     program, = parse_toot(program_text)
-    return eval_program(program)
+    return terp.eval_program(program)
 
 ## run(open('factorial.toot').read())
 #. 120
