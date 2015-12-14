01e3aa552ebcabd63575d55f88ac7ae70bb42a6b
Darius Bacon <darius@wry.me>
2015-12-14 16:05:15 -0500

    add terp2

--- testme.py
+++ testme.py
@@ -3,7 +3,8 @@ Smoke test
 """
 
 from parse import parse_toot
-import terp0 as terp
+#import terp0 as terp
+import terp2_split_env as terp
 
 def run(program_text):
     program, = parse_toot(program_text)
