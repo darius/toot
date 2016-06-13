72f5005cf034bb4c742bcca9f0b7fb3b318afe6b
Darius Bacon <darius@wry.me>
2016-06-12 21:22:26 -0400

    I guess it's clearer to line up parens like this.

--- stack_example.py
+++ stack_example.py
@@ -6,7 +6,7 @@ Prim2(operator.mul,                                             []
       Call('factorial',
            (Prim2(operator.sub,
                   Variable('n'),        push_variable 0         [5, 5]
-                  Constant(1)),)))),)   push_constant 1         [5, 5, 1]
-                                        do_op2 operator.sub     [5, 4]
-                                        call (0, 1)             [5, 24]
-                                        do_op2 operator.mul     [120]
+                  Constant(1)           push_constant 1         [5, 5, 1]
+                 )                      do_op2 operator.sub     [5, 4]
+            ,))                         call (0, 1)             [5, 24]
+     )                                  do_op2 operator.mul     [120]
