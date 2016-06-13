1218eb6d426618fe571cf9167bef230eb79a1b42
Darius Bacon <darius@wry.me>
2016-06-12 21:06:39 -0400

    tweak

--- parse.py
+++ parse.py
@@ -3,21 +3,21 @@
 from parson import Grammar
 import abstract_syntax
 
-toot_grammar = r""" program.
+toot_grammar = r""" program :end.
 
-program: defn* :hug print :end              :Program.
+program: defn* :hug print                   :Program.
 print:   "print" exp.
 defn:    "def" id '(' params ')' ':' stmt   :Definition.
 stmt:    "return" exp.
 
 exp:     exp1 ("if" exp1 "else" exp :If)?.
 exp1:    exp2 ( '<'   exp2 :Less
-              | '=='  exp2 :Eq)?.
+              | '=='  exp2 :Eq  )?.
 exp2:    exp3 (  '+'  exp3 :Add
-               | '-'  exp3 :Sub)*.
+               | '-'  exp3 :Sub )*.
 exp3:    exp4 (  '*'  exp4 :Mul
                | '/'  exp4 :Div
-               | '%'  exp4 :Mod)*.
+               | '%'  exp4 :Mod )*.
 
 exp4:    '(' exp ')'
       |  '-' exp4             :Neg
@@ -28,7 +28,7 @@ exp4:    '(' exp ')'
 params:     id ** ','         :hug.
 arguments:  exp1 ** ','       :hug.
 
-id:      /([A-Za-z_][A-Za-z_0-9]*)\b/.
+id:      /([A-Za-z_][A-Za-z_0-9]*)/.
 
 FNORD ~: /\s*/.
 """
